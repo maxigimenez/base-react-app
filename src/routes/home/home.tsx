a114b9c03064e623349f2c275cc8d75debd78bd5
Maxi Gimenez <gimenez.maxi@gmail.com>
2019-12-22 18:01:59 +0100

    feat: add axios support

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { api } from '../../helper/axios';
 
 export const Home: React.FunctionComponent = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetch = async () => {
+      const { data } = await api.get('https://sheetapi.co/apis/8LZaJRD5vCK7wT5y1dNLM6/raw');
+      setData(data);
+    }
+    fetch();
+  }, []);
+
   return <div>
     Welcome!
+    <hr />
+    Fetched data: { data.length }
   </div>;
 }
